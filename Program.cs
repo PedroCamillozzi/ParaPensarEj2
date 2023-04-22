a06d6c6e7236b5f9ba1b5f0332b0e4ef17f72193
PedroCamillozzi <pedrocamillozzi01@gmail.com>
2023-04-22 00:15:20 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ParaPensarEj2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int año;
+
+            Console.WriteLine("Ingrese un año");
+            año = Convert.ToInt32(Console.ReadLine());
+            if ((año % 400) == 0)
+            {
+                Console.WriteLine("El año es bisiesto");
+            }
+            else
+            {
+                if ((año % 4) == 0)
+                {
+                    if ((año % 100) != 0)
+                    {
+                        Console.WriteLine("El año es bisiesto");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El año no es bisiesto");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("El año no es bisiesto");
+                }
+            }
+            Console.ReadKey();
+        }   
+    }
+}
